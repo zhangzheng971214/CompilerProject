23b1a317f7c78dca03c4d0808f299c74f363ba75
Ranger Shaw <15307130260@fudan.edu.cn>
2018-12-17 13:58:24 +0800

    add exception handler

---
+++ src/main/ExceptionHandler.java
@@ -0,0 +1,31 @@
+package main;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class ExceptionHandler {
+
+    private int exceptionNum;
+
+    ExceptionHandler() {
+        exceptionNum = 0;
+    }
+
+    public void printException(String txt) {
+        exceptionNum++;
+        System.err.println(txt);
+    }
+
+    public int addException() {
+        exceptionNum++;
+        return exceptionNum;
+    }
+
+    public void checkException(){
+        if(exceptionNum!=0){
+            System.err.println(exceptionNum + " exceptions found!");
+            System.err.println("Program will exit!");
+            System.exit(exceptionNum);
+        }
+    }
+}
--- src/main/SyntaxErrorListener.java
+++ src/main/SyntaxErrorListener.java
@@ -3,11 +3,19 @@ package main;
 import org.antlr.v4.runtime.*;
 
 public class SyntaxErrorListener extends BaseErrorListener {
+    private ExceptionHandler _exceptionHandler;
+
+    SyntaxErrorListener(ExceptionHandler exceptionHandler){
+        super();
+        _exceptionHandler=exceptionHandler;
+    }
+
     @Override
     public void syntaxError(Recognizer<?, ?> recognizer,
                             Object offendingSymbol,
                             int line, int charPositionInLine,
                             String msg, RecognitionException e) {
+        _exceptionHandler.addException();
         System.err.println("line " + line + ":" + charPositionInLine + " " + msg);
         underlineError(recognizer, (Token) offendingSymbol, line, charPositionInLine);
     }
