7a4ed7fb2b698ed8b90015728933fecb74f843c4
Zhang Zheng <1226224047@qq.com>
2018-12-18 13:15:00 +0800

    在类声明的结点中，增加对“循环继承”错误的检查，修改classNode以支持此功能

--- src/main/classNode.java
+++ src/main/classNode.java
@@ -1,41 +1,58 @@
 package main;
+
 import com.sun.istack.internal.NotNull;
 
 import java.util.*;
 
 //定义抽象语法树中的每个结点类，实际上就是每个规则rule所对应的类，其中维护了结点
 //中的符号表信息
-public class classNode extends Symbol implements Scope{  //可将结点也看作上一层结点作用域内的一个符号，
-                                                    // 结点中的scope通过继承Scope来实现
+public class classNode extends Symbol implements Scope {  //可将结点也看作上一层结点作用域内的一个符号，
+    // 结点中的scope通过继承Scope来实现
     private classNode parent;
     private Scope upperScope;
     public Map<String, Symbol> symTable = new HashMap<String, Symbol>(); //当前结点作用域中的符号表
     private boolean valid = true; //标明当前结点是否valid, 默认true，TODO：这种默认参数值的用法
+    private boolean hasParent; //用于区分有无parent
 
     //construction func
-    public classNode(String name, classNode parent, Scope upper, boolean valid){ //带parent结点的构造函数
+    public classNode(String name, classNode parent, Scope upper, boolean valid) { //带parent结点的构造函数
         super(name);
         this.parent = parent;
         this.upperScope = upper;
         this.valid = valid;
+        this.hasParent = true;
     }
-    public classNode(String name, classNode parentNode, Scope upper){
+
+    public classNode(String name, classNode parentNode, Scope upper) {
         super(name);
         this.parent = parentNode;
         this.upperScope = upper;
+        this.hasParent = true;
     }
-    public classNode(String name, @NotNull String noParentMsg, Scope upper, boolean valid){ //没有parent结点的构造函数
+
+    public classNode(String name, @NotNull String noParentMsg, Scope upper, boolean valid) { //没有parent结点的构造函数
         super(name);
         this.upperScope = upper;
         this.valid = valid;
+        this.hasParent = false;
     }
-    public classNode(String name, @NotNull String noParentMsg, Scope upper){ //没有parent结点的构造函数
+
+    public classNode(String name, @NotNull String noParentMsg, Scope upper) { //没有parent结点的构造函数
         super(name);
         this.upperScope = upper;
+        this.hasParent = false;
+    }
+
+    public classNode getParent() {
+        return parent;
+    }
+
+    public boolean hasParent(){
+        return hasParent;
     }
 
     //实现scope中的接口函数
-    public String getName(){ //TODO:子类中这个方法是否可以不用实现？ ---不行，作为scope接口，current直接声明为scope类型
+    public String getName() { //TODO:子类中这个方法是否可以不用实现？ ---不行，作为scope接口，current直接声明为scope类型
         return super.getName();
     }
 
@@ -47,17 +64,18 @@ public class classNode extends Symbol implements Scope{  //可将结点也看作
         return upperScope;
     }
 
-    public void addSymbol(Symbol sym){
+    public void addSymbol(Symbol sym) {
         symTable.put(sym.getName(), sym);
     }
 
-    public boolean isValid(){
+    public boolean isValid() {
         return valid;
     }
 
     public Symbol findSymbol(String name) { //遍历当前结点符号表，根据name，找到symbol对象
         return symTable.get(name);
     }
+
     //For Test
     public Map<String, Symbol> getSym() {
         return symTable;
--- src/main/scopeChecker.java
+++ src/main/scopeChecker.java
@@ -1,33 +1,34 @@
 package main;
+
 import main.*;
 import main.gen.*;
 
 import java.util.*;
 
 //Constructor of Scope Tree, including Scopes and Symbol Tables
-public class scopeChecker extends MiniJavaBaseListener{ //建立每一个rule的作用域树，并保存好每个结点的符号表，同时初步检查作用域的语义错误
+public class scopeChecker extends MiniJavaBaseListener { //建立每一个rule的作用域树，并保存好每个结点的符号表，同时初步检查作用域的语义错误
     private Map<String, classNode> classNodes; //保存AST中所有的结点对象
     private Scope current; //记录当前处理的作用域，可为class也可为method
     private ExceptionHandler exceptionHandler;
 
-    public scopeChecker(Map<String, classNode> classNodes, Scope scope, ExceptionHandler exceptionHandler){ //构造函数
+    public scopeChecker(Map<String, classNode> classNodes, Scope scope) { //构造函数
         this.classNodes = classNodes;
         this.current = scope; //指代goal
-        this.exceptionHandler=exceptionHandler;
+        this.exceptionHandler = exceptionHandler;
     }
 
-    public void exitScope(){
+    public void exitScope() {
         //退出当前结点作用域
         current = current.getUpperScope(); //转到其parent结点的Scope,classNode也可认为是Scope
     }
 
     //重写MiniJavaBaseListener中的enter方法
     @Override
-    public void enterMainClass(MiniJavaParser.MainClassContext ctx){
+    public void enterMainClass(MiniJavaParser.MainClassContext ctx) {
         //将此结点添加至classNodes中,并加入符号表
         String nodeName = ctx.name.getText();
         classNode mainClass = new classNode(nodeName, "<No Parent>", current); //建立Node类，mianClass是没有parent
-                                                                                            //但是有upperScope
+        //但是有upperScope
         classNodes.put(nodeName, mainClass); //结点加入到Nodes
 
         current.addSymbol(mainClass); //此结点作为符号加入到current符号表中
@@ -35,31 +36,57 @@ public class scopeChecker extends MiniJavaBaseListener{ //建立每一个rule的
     }
 
     @Override
-    public void exitMainClass(MiniJavaParser.MainClassContext ctx){exitScope();}
+    public void exitMainClass(MiniJavaParser.MainClassContext ctx) {
+        exitScope();
+    }
 
     @Override
-    public void enterClassDeclaration(MiniJavaParser.ClassDeclarationContext ctx){
+    public void enterClassDeclaration(MiniJavaParser.ClassDeclarationContext ctx) {
         //将此结点添加至classNodes中
         //TODO:考察parent结点是否是current结点
         String nodeName = ctx.name.getText();
         boolean valid = true; //TODO:current.isValid();
-        classNode parent = ctx.parent!=null ? classNodes.get(ctx.parent.getText()) : null; //获得parent的名
+        classNode classDeclaration;
         //类声明的过程中需要考察类是否重复定义
-        if(classNodes.containsKey(nodeName)) {
+        if (classNodes.containsKey(nodeName)) {
             //System.out.println("类名重复定义");//TODO:错误输出
             exceptionHandler.addException(ctx.name, "类名重复定义");
             valid = false;
         }
-        classNode classDeclaration = new classNode(nodeName, parent, current, valid); //upperScope默认为parent
-        if(valid) { //TODO:需不需要考察valid？
+        //构造class结点
+        if (ctx.parent != null) {
+            classDeclaration = new classNode(nodeName, classNodes.get(ctx.parent.getText()), current, valid); //upperScope默认为parent
+        } else {
+            classDeclaration = new classNode(nodeName, "<No Parent>", current, valid);
+        }
+        //结点添加到classNode中，以及作为符号加入current的作用域
+        if (valid) { //TODO:需不需要考察valid？
             current.addSymbol(classDeclaration);
             classNodes.put(nodeName, classDeclaration); //添加到全局的classNodes中
         }
         current = classDeclaration; //当前处理作用域设为mainClass的Scope
+
+        //此处在类声明时检查是否有循环继承
+        if (ctx.parent != null) {
+            //TODO:Check Code!!!
+            //不断往上找parent，如果找到自己，则报错；如果找到空parent,则跳出
+            classNode parent = classNodes.get(ctx.parent.getText());
+            while (true) {
+                if (nodeName.equals(parent.getName())) {
+                    System.out.println("循环继承！");//TODO:错误输出
+                    break;
+                }
+                if (!parent.hasParent()) //TODO:怎么判断no parent
+                    break;
+                parent = parent.getParent();
+            }
+        }
     }
 
     @Override
-    public void exitClassDeclaration(MiniJavaParser.ClassDeclarationContext ctx) {exitScope(); }
+    public void exitClassDeclaration(MiniJavaParser.ClassDeclarationContext ctx) {
+        exitScope();
+    }
 
     @Override
     public void enterVarDeclaration(MiniJavaParser.VarDeclarationContext ctx) {
@@ -68,56 +95,58 @@ public class scopeChecker extends MiniJavaBaseListener{ //建立每一个rule的
         String varType = ctx.vtype.getText(); //对于变量声明，需要记录其symbol的名字和类型
         boolean valid = current.isValid();
         //变量的声明过程中需要检查其是否已经在当前作用域下被重复声明
-        if(current.findSymbol(varName) != null){
+        if (current.findSymbol(varName) != null) {
             //System.out.println("变量名重复定义");//TODO:错误输出
             exceptionHandler.addException(ctx.name, "变量名重复定义");
             valid = false;
         }
-        if(valid){
+        if (valid) {
             Symbol var = new Symbol(varName, varType); //新建Symbol对象
             current.addSymbol(var);
         }
     }
 
     @Override
-    public void enterMethodDeclaration(MiniJavaParser.MethodDeclarationContext ctx){
+    public void enterMethodDeclaration(MiniJavaParser.MethodDeclarationContext ctx) {
         //考察method声明的作用域检查，检查方法是否已被声明
         String nodeName = ctx.name.getText();
         String returnType = ctx.rtype.getText();
         boolean valid = current.isValid();
 
         //检查方法是否已被声明，即current符号表中是否有method同名符号
-        if(current.findSymbol(nodeName) != null){
+        if (current.findSymbol(nodeName) != null) {
             //System.out.println("方法名重复定义");//TODO:错误输出
             exceptionHandler.addException(ctx.name, "方法名重复定义");
             valid = false;
         }
         methodNode method = new methodNode(nodeName, returnType, current, valid);
-        if(valid){
+        if (valid) {
             current.addSymbol(method);
         }
         current = method;
     }
 
     @Override
-    public void exitMethodDeclaration(MiniJavaParser.MethodDeclarationContext ctx){exitScope();}
+    public void exitMethodDeclaration(MiniJavaParser.MethodDeclarationContext ctx) {
+        exitScope();
+    }
 
     @Override
-    public void enterFormalParameters(MiniJavaParser.FormalParametersContext ctx){
+    public void enterFormalParameters(MiniJavaParser.FormalParametersContext ctx) {
         //识别到形参结点时，将形参作为符号添加到method中去
         String paraName = ctx.name.getText();
         String paraType = ctx.ptype.getText();
         boolean valid = current.isValid();
 
         //检查形参是否重复
-        if(current.getNode().findSymbol(paraName) != null){
+        if (current.getNode().findSymbol(paraName) != null) {
             System.out.println("形参重复定义");//TODO:错误输出
             exceptionHandler.addException(ctx.name, "形参重复定义");
             valid = false;
         }
-        if(valid){
+        if (valid) {
             Symbol para = new Symbol(paraName, paraType);
-            ((methodNode)current.getNode()).addPara(para); //addPara()方法是methodNode特有的
+            ((methodNode) current.getNode()).addPara(para); //addPara()方法是methodNode特有的
         }
     }
 
